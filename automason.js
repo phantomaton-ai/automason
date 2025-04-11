d6d702e9fcbbb66f174d8b6ee167b7ced24accf0
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-04-11 20:55:27 +0000

    Updated by Phantomaton

--- automason.js
+++ automason.js
@@ -1,13 +1,13 @@
-import gallows from 'gallows';
 import phantomaton from 'phantomaton';
 import execution from 'phantomaton-execution';
 import plugins from 'phantomaton-plugins';
+import util from './util.js';
 
 export default (action, attributes, body) => phantomaton(body, {
   install: ['phantomaton-execution', 'phantomaton-projects', { install: [
     plugins.define(plugins.start).with(
       execution.command
-    ).as((commands) => () => gallows(commands).execute(
+    ).as((commands) => () => util.gallows(commands).execute(
       action, attributes, body
     ))
   ] }]
