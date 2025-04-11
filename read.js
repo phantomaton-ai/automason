a28f0e70b42c8e97a1bc1275c45981f32ee5d57d
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-04-11 20:33:54 +0000

    Updated by Phantomaton

---
+++ read.js
@@ -0,0 +1,23 @@
+export default async function read(stream) {
+  return new Promise((resolve) => {
+    let body = '';
+    
+    // Very short timeout to minimize latency
+    const timeout = setTimeout(() => {
+      resolve(body);
+    }, 20);
+
+    stream.on('readable', () => {
+      clearTimeout(timeout);
+      let chunk;
+      while ((chunk = stream.read()) !== null) {
+        body += chunk;
+      }
+    });
+
+    stream.on('end', () => {
+      clearTimeout(timeout);
+      resolve(body);
+    });
+  });
+}
